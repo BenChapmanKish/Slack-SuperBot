a3c1d03bb76a9a2f58d9e979169da0a5a5224441
BenChapmanKish <ben.chapmankish@gmail.com>
2016-08-25 00:26:09 -0400

    Creating base for code and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 #UtilityBot
 ###A multi-purpose bot for Slack
+
+This bot will connect to slack and have many different tasks it can perform.
+
+Dependencies:
+
+* [@slackhq python-rtmbot](https://github.com/slackhq/python-rtmbot)
+* [@google gdata-python-client](https://github.com/google/gdata-python-client)
+
+-----
+
+Features to add:
+
+* Posting to anonymous channel (use unique identifiers)
+* Find random song from youtube playlist
--- utilitybot.py
+++ utilitybot.py
@@ -1,5 +1,23 @@
 # coding: utf-8
 
-# Need to decide between these:
-from slacker import Slacker
+import sys, json
+
 import rtmbot
+
+import gdata.youtube
+import gdata.youtube.service
+
+YT_CLIENT_ID = "Slack-UtilityBot"
+
+with open('../CREDENTIALS.json') as f:
+	CREDENTIALS = f.read()
+
+yt_service = gdata.youtube.service.YouTubeService()
+
+# Turn on HTTPS/SSL access.
+# Note: SSL is not available at this time for uploads.
+yt_service.ssl = True
+yt_service.developer_key = CREDENTIALS['youtube']['key']
+yt_service.client_id = YT_CLIENT_ID 
+
+slack_key = CREDENTIALS['slack']['key']
