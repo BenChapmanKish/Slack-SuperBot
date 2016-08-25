bf784faec706018fd751b979abd4ed93a15b3b23
BenChapmanKish <ben.chapmankish@gmail.com>
2016-08-25 00:38:11 -0400

    Fixed some bugs, added some comments, you know how it is

--- utilitybot.py
+++ utilitybot.py
@@ -2,15 +2,40 @@
 
 import sys, json
 
-import rtmbot
-
 import gdata.youtube
 import gdata.youtube.service
 
+# This is a plugin for python-rtmbot
+# If one would prefer, we could use SlackClient instead
+
 YT_CLIENT_ID = "Slack-UtilityBot"
 
-with open('../CREDENTIALS.json') as f:
-	CREDENTIALS = f.read()
+# Put your slack and youtube credentials in a json file
+with open('../credentials.json') as f:
+	CREDENTIALS = json.loads(f.read())
+
+crontable = []
+outputs = []
+
+########################################
+# ANONYMOUS CHANNEL WITH UNIQUE IDENTIFIERS
+
+# https://api.slack.com/bot-users
+
+# This hasn't been made yet
+
+########################################
+
+
+
+
+########################################
+# YOUTUBE PLAYLIST RANDOM SONG
+
+# Use these APIs:
+# https://developers.google.com/youtube/1.0/developers_guide_python
+# https://developers.google.com/youtube/v3/docs/search/list
+# https://developers.google.com/youtube/v3/docs/playlistItems/list
 
 yt_service = gdata.youtube.service.YouTubeService()
 
@@ -21,3 +46,5 @@ yt_service.developer_key = CREDENTIALS['youtube']['key']
 yt_service.client_id = YT_CLIENT_ID 
 
 slack_key = CREDENTIALS['slack']['key']
+
+########################################
