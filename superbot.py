f58a3c225cd42163c1cfc1e3337e707da097436e
BenChapmanKish <ben.chapmankish@gmail.com>
2016-08-31 22:46:49 -0400

    Base code is technically finished but don't try to use it until I document it and explain how

--- superbot.py
+++ superbot.py
@@ -15,15 +15,10 @@ sys.dont_write_bytecode = True
 this_dir = os.path.dirname(os.path.realpath(__file__))
 
 '''
-Idea for reorganizing this framework:
-(CURRENTLY BEING IMPLEMENTED)
-
-A SuperBot class that does all the API calls and stuff
-would be created, recycling code from RtmBot.
-
-Each plugin has a function that's called when the
-slack client's rtm_read receives input. Said function
-would be passed the SuperBot instance and the event data.
+Documentation for this file will be provided later.
+I'm a bit too busy right now, sorry!
+I suggest not trying to develop for SuperBot just
+yet, until I can explain how.
 '''
 
 
@@ -38,8 +33,12 @@ class SuperBot(object):
 
 		# set working directory for loading plugins or other files
 		self.directory = self.config.get('base_path', this_dir)
-		if not self.directory.startswith('/'):
-			path = '{}/{}'.format(os.getcwd(), self.directory)
+		
+		if self.directory.startswith('~'):
+			path = os.path.join(os.path.expanduser('~'), self.directory)
+			self.directory = os.path.expanduser(path)
+		elif not self.directory.startswith('/'):
+			path = os.path.join(os.getcwd(), self.directory)
 			self.directory = os.path.abspath(path)
 
 		# establish logging
@@ -52,7 +51,8 @@ class SuperBot(object):
 
 		# initialize stateful fields
 		self.last_ping = 0
-		self.plugins = []
+		self.plugin_names = []
+		self.plugin_instances = []
 		self.slack_client = None
 
 	def _dbg(self, debug_string):
@@ -69,7 +69,7 @@ class SuperBot(object):
 		self.load_plugins()
 		while True:
 			for reply in self.slack_client.rtm_read():
-				self.event_handlers(reply)
+				self.eventHandlers(reply)
 			self.autoping()
 			time.sleep(.1)
 
@@ -88,32 +88,40 @@ class SuperBot(object):
 			self.slack_client.server.ping()
 			self.last_ping = now
 
-	def event_handlers(self, data):
+	def eventHandlers(self, data):
 		if "type" in data:
 			self._dbg("got {}".format(data["type"]))
-			for plugin in self.plugins:
+			self.handle_event(data)
+			for plugin in self.plugin_instances:
 				if self.debug:
-					plugin.handle_event(data)
+					plugin.handleEvent(data)
 				else:
 					try:
-						plugin.handle_event(data)
+						plugin.handleEvent(data)
 					except Exception:
-						logging.exception("problem in module {} {}".format(function_name, data))
+						logging.exception("problem in module {} {}".format(plugin, data))
 
-	def send_message(self, channel, message=None):
+	def sendMessage(self, channel, message=None):
 		channel = self.slack_client.server.channels.find(channel)
 		if channel is not None and message is not None:
 			channel.send_message(message)
 			return True
 		return False
 
-	def api_call(self, method, kwargs={}):
+	def apiCall(self, method, kwargs={}):
 		if method is not None:
 			self.slack_client.server.api_call(method, **kwargs)
 			return True
 		return False
 
-	def find_plugins(self):
+	def loadPluginInstances(self):
+		self.plugin_instances = []
+		for name in self.plugin_names:
+			module = __import__(name)
+			instance = module.Plugin(self)
+			self.plugin_instances.append(instance)
+
+	def findPlugins(self):
 		sys.path.insert(0, self.directory + '/plugins/')
 		for plugin in glob.glob(self.directory + '/plugins/*'):
 			sys.path.insert(1, plugin)
@@ -121,91 +129,15 @@ class SuperBot(object):
 		for plugin in glob.glob(self.directory + '/plugins/*.py'):
 			logging.info(plugin)
 			name = plugin.split('/')[-1][:-3]
-			self.plugins.append(name)
+			self.plugin_names.append(name)
 
 
 class Plugin(object):
-	"""
-	ABOUT TO BE REWRITTEN
-	"""
 	def __init__(self, superbot):
-		'''
-		A plugin in initialized with:
-			- name (str)
-			- plugin config (dict) - (from the yaml config)
-				Values in config:
-				- DEBUG (bool) - this will be overridden if debug is set in config for this plugin
-		'''
 		self.sb = superbot
-		self.jobs = []
-		self.module = __import__(name)
-		self.module.config = plugin_config
-		self.debug = self.module.config.get('DEBUG', False)
-		self.register_jobs()
-		self.outputs = []
-		self.api_calls = []
-		if 'setup' in dir(self.module):
-			self.module.setup()
 
-	def register_jobs(self):
-		if 'crontable' in dir(self.module):
-			for interval, function in self.module.crontable:
-				self.jobs.append(CronJob(interval, eval("self.module." + function), self.debug))
-			logging.info(self.module.crontable)
-			self.module.crontable = []
-		else:
-			self.module.crontable = []
-
-	def do(self, function_name, data):
-		if function_name in dir(self.module):
-			if self.debug is True:
-				# this makes the plugin fail with stack trace in debug mode
-				eval("self.module." + function_name)(data)
-			else:
-				# otherwise we log the exception and carry on
-				try:
-					eval("self.module." + function_name)(data)
-				except Exception:
-					logging.exception("problem in module {} {}".format(function_name, data))
-		if "catch_all" in dir(self.module):
-			if self.debug is True:
-				# this makes the plugin fail with stack trace in debug mode
-				self.module.catch_all(data)
-			else:
-				try:
-					self.module.catch_all(data)
-				except Exception:
-					logging.exception("problem in catch all: {} {}".format(self.module, data))
-
-	def do_jobs(self):
-		for job in self.jobs:
-			job.check()
-
-	def do_output(self):
-		output = []
-		while True:
-			if 'outputs' in dir(self.module):
-				if len(self.module.outputs) > 0:
-					logging.info("output from {}".format(self.module))
-					output.append(self.module.outputs.pop(0))
-				else:
-					break
-			else:
-				self.module.outputs = []
-		return output
-
-	def do_api_calls(self):
-		api_calls = []
-		while True:
-			if 'api_calls' in dir(self.module):
-				if len(self.module.api_calls) > 0:
-					logging.info("api call from {}".format(self.module))
-					api_calls.append(self.module.api_calls.pop(0))
-				else:
-					break
-			else:
-				self.module.api_calls = []
-		return api_calls
+	def handleEvent(data):
+		raise NotImplementedError
 
 def main():
 	bot = SuperBot()
