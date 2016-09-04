0957ec7bdafe710d062b1f968ef8cb327eede1e2
BenChapmanKish <ben.chapmankish@gmail.com>
2016-09-03 20:51:13 -0700

    Fixed auto-reload script

--- run-bot.sh
+++ run-bot.sh
@@ -6,16 +6,17 @@
 # to connect with ssh, and the ssh key must be kept in a
 # local, loaded keychain (e.g. with ssh-add)
 
-git pull origin master
-python3 superbot.py
+git pull origin master &> run-bot.log
+python3 superbot.py &> run-bot.log
 PID_FILE=$(cat superbot_pid_file.txt)
 while true; do
-	git fetch origin
+	git fetch origin &> run-bot.log
 	NUM_DIFF=$(git rev-list HEAD...origin/master --count)
 	if [[ ! "$NUM_DIFF" -eq 0 ]]; then
-		kill $(cat PID_FILE)
-		git merge origin/master
-		python3 superbot.py
+		echo "Changed detected at $(date +'%y/%m/%d %T'), reloading superbot"
+		kill $(cat $PID_FILE) &> run-bot.log
+		git merge origin/master &> run-bot.log
+		python3 superbot.py &> run-bot.log
 		PID_FILE=$(cat superbot_pid_file.txt)
 	fi
 	sleep 10
